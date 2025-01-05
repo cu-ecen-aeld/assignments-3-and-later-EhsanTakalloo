faf3183959e38559c9296f232039b3a23153f82b
Ehsan Takaloo <e.takaloo@crouse.ir>
2025-01-05 10:46:00 +0330

    fix(app): Update test_validate_my_username in Test_validate_username.c
    Signed-off-by: Ehsan Takaloo <e.takaloo@crouse.ir>

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,7 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *hardCodeUsername = my_username();
+    const char *confFileUsername = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardCodeUsername, confFileUsername, "The username from the conf file matches the hard coded username");
 }
